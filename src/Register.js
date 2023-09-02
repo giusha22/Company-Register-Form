009407a9510ccc6cc101099781e1c9f30f274435
Giorgi Mtsituri <giorgimtsituri42@gmail.com>
2023-09-02 09:42:13 +0400

    some change

--- src/Register.js
+++ src/Register.js
@@ -3,7 +3,6 @@ import { faCheck, faTimes, faInfoCircle } from "@fortawesome/free-solid-svg-icon
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "./api/axios";
 
-//hello
 
 const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/;
 const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
